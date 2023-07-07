046e70b8cbc88a3ceb7a73a41a7bc215f68635ab
zac <17621155553@163.com>
2023-07-07 16:50:49 +0800

    paix

--- src/main/java/com/trjst/util/ServerConfig.java
+++ src/main/java/com/trjst/util/ServerConfig.java
@@ -8,7 +8,8 @@ public class ServerConfig implements Serializable {
 	private static final long serialVersionUID = 19386536267359832L;
 
 	private static String APP_ID ="wxe3bf6f6c2d88acd6";//appid
-	private static String APP_SECRET ="fe6dc6e45af08a06fcf7355289de49dd";//秘钥
+	//private static String APP_SECRET ="fe6dc6e45af08a06fcf7355289de49dd";//秘钥
+	private static String APP_SECRET ="1b004a780c244e8cc988b8474e90c053";//秘钥
 	private static String partnerId ="1615024399";//商户ID
 	private static String partnerSecert ="trjishitong12111trjishitong12111";//商户秘钥
 	private static String paynotify ="https://www.trjst.com/jst/wxpayquery";//回调地址
